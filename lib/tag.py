b0f621c64718c15c80635c5e4e6f777c7d10cdff
Chris Kuehl <chris@techxonline.net>
2014-04-16 21:40:45 -0700

    Add tag.py helper program for reading media tags

---
+++ lib/tag.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python2
+"""Helper program whose only purpose is to print tags from a media file. Relies
+on the mutagen library, which is currently only available for Python 2.
+
+The rest of the musicman project invokes this as a binary (rather than, for
+example, importing it) so that python3 compatibility can be maintained."""
+
+import json
+import sys
+
+import mutagen
+
+if __name__ == "__main__":
+	if len(sys.argv) != 2:
+		sys.exit("usage: tag.py [path to file]")
+
+	path = sys.argv[1]
+	f = mutagen.File(path, easy=True)
+
+	if not f:
+		sys.exit("unable to read file")
+
+	print json.dumps(dict((k, v) for (k, v) in f.iteritems()), indent=4)
